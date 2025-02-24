9ae38fa4757bdf3b6d2aaa307f2f16672f377c1c
Octavio Michel <octaviohounye123@gmail.com>
2025-02-24 10:30:15 +0100

    update .env

--- config/db.js
+++ config/db.js
@@ -1,16 +1,9 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 3306,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'mysql',
     logging: false,
-  }
-);
-
-module.exports = sequelize;
+  });
+  
+  module.exports = sequelize;
